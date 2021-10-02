c91a6fcb188e30b28dfc45a3648f1cf0ee7d3010
DLJin <danielji@umich.edu>
2021-10-01 23:03:39 -0700

    Added PlayerCharacter shooting

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class PlayerCharacter : MonoBehaviour
 {
+    public float movementSpeed = 5.0f;
+    public float attackSpeed = 3.0f;
+    public GameObject projectile;
+    public float projectileSpeed = 20.0f;
+
+    private float timeSinceLastAttack = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -12,22 +19,37 @@ public class PlayerCharacter : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {       
+    {
+        calculateMovement();
+
+        if (timeSinceLastAttack > 1.0f/attackSpeed && Input.GetKey(KeyCode.Space)) {
+            Quaternion rotation = new Quaternion(0, 0, 90, 90);
+            GameObject go = Instantiate(projectile, transform.position, rotation);
+            go.GetComponent<Rigidbody2D>().velocity = Vector3.right * projectileSpeed;
+            timeSinceLastAttack = 0.0f;
+        }
+        else {
+            timeSinceLastAttack += Time.deltaTime;
+        }
+
+    }
+
+    void calculateMovement() {
         Rigidbody2D rb = GetComponent<Rigidbody2D>();
 
         Vector3 newVelocity = new Vector3(0, 0);
 
         if (Input.GetKey(KeyCode.W)) {
-            newVelocity.y = newVelocity.y + 5;
+            newVelocity.y = newVelocity.y + movementSpeed;
         }
         if (Input.GetKey(KeyCode.A)) {
-            newVelocity.x = newVelocity.x - 5;
+            newVelocity.x = newVelocity.x - movementSpeed;
         }
         if (Input.GetKey(KeyCode.S)) {
-            newVelocity.y = newVelocity.y - 5;
+            newVelocity.y = newVelocity.y - movementSpeed;
         }
         if (Input.GetKey(KeyCode.D)) {
-            newVelocity.x = newVelocity.x + 5;
+            newVelocity.x = newVelocity.x + movementSpeed;
         }
 
         rb.velocity = newVelocity;
